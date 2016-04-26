f26350410200f5e1d660299ff91bc1d057f6ef5a
Peter Richards <peter.jrichards@yahoo.com>
2016-04-26 18:47:25 +0700

    added logic for posts to feed

--- web/controllers/api/feed_controller.ex
+++ web/controllers/api/feed_controller.ex
@@ -1,7 +1,28 @@
 defmodule LocIm.Api.FeedController do
   use LocIm.Web, :controller
+  import Ecto.Query
 
+  @per_page 20
+
+  def index(conn, %{"page_number" => page_number} ) do
+    # Get Posts by page
+    posts = String.to_integer("#{page_number}")
+    |> feed_posts
+    # Render Posts
+    conn
+    |> assign(:posts, posts)
+    |> render("index.json")
+  end
+
+  # If no page number is included, set page_number to 1
   def index(conn, _) do
-    render conn, "index.json"
+    index(conn, %{"page_number" => 1})
+  end
+
+  # Logic for what posts to show
+  defp feed_posts(page_number) do
+    skip = (page_number - 1) * @per_page
+    query = from p in LocIm.Post, order_by: [desc: p.inserted_at], offset: ^skip, limit: @per_page, preload: :user
+    posts = LocIm.Repo.all query
   end
 end
